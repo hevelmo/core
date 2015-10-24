b4bb148d4eca19e8ffb2847270a1dc839c2c4c9d
hevelmo <hevelmo060683@gmail.com>
2015-10-24 13:27:55 -0500

    upload phpobjectjs

---
+++ phpobjectjs/to_object/domEl.php
@@ -0,0 +1,260 @@
+<?php
+
+function domEl() {
+    return array(
+        'a_return_index' => 'a#return_index',
+        'a_return_nav_index' => 'a#return-index',
+        'ide_return_button' => '#back',
+
+        'link_go_agencies' => 'a#go-agencies',
+        'link_go_inventories' => 'a#go-inventories',
+        'ide_go_inventories' => 'div#id-go-inventories',
+
+        //GENERAL HI DIVS
+        'head_content_head' => 'head#content-head',
+        'head_recurrent' => 'head',
+        'div_tags_share' => 'div#tags-share',
+        'div_recurrent_body' => 'body',
+        'div_hidden_inputs_session' => 'div#hidden-inputs-session',
+        'div_hidden_inputs_temporal' => 'div#hidden-inputs-temporal',
+        'div_current_temporal_filters' => 'div#temporal-filters',
+
+        'input_current_hidden_folder_agencia' => 'input#hidden_folder_agencia',
+        'input_current_hidden_logo_agencia' => 'input#hidden_logo_agencia',
+        'input_current_hidden_name_agencia' => 'input#hidden_name_agencia',
+        'input_current_hidden_address_agencia' => 'input#hidden_address_agencia',
+
+        'input_hidden_mapa' => 'input#hidden_mapa',
+
+        'input_current_hidden_category' => 'input#hidden_category',
+        'input_current_hidden_marc' => 'input#hidden_marc',
+        'input_current_hidden_model' => 'input#hidden_model',
+        'input_current_hidden_agen_pre_owned_name' => 'input#hidden_agen_pre_owned_name',
+        'input_current_hidden_agen_pre_owned_id' => 'input#hidden_agen_pre_owned_id',
+
+        'input_current_hidden_agen_news_name' => 'input#hidden_agen_news_name',
+
+        'div_recurrent' => 'div#content-temporal-interactive',
+        'div_container_start_page_header' => 'div#container-start-page-header',
+        //BANNER
+        'img_banner' => '#img-banner',
+        // CONTENT HOME SLIDER
+        'div_content_revolution_slider' => 'div#content-revolution-slider',
+        // CONTENT MARCS LOGOS
+        'div_content_marcs_logos' => 'div#content-marcs-logos',
+        // LOGO ACTIONS
+        'agencie_action_seminuevo' => '.agn-act-seminuevo',
+        'agencie_action_nuevo' => '.agn-act-nuevo',
+        'agencie_action_taller' => '.agn-act-taller',
+        'agencie_action_rentas' => '.agn-act-rentas',
+
+        // RENTAILS
+        'ide_go_agn_rent_u_save' => 'div#id-go-u-save',
+        'div_recurrent_rental' => 'div#content-rental',
+        'div_recurrent_rental_info' => 'div#content-rental-info',
+        'div_recurrent_rental_agencies' => 'div#content-rental-agencies',
+
+        'div_content_usave' => 'div#rentail-usave',
+        // COUNTUP
+        'div_content_counters' => 'div#content-counters',
+        // FEATURES
+        'div_content_fullwidth_features' => 'div#content-fullwidth-features',
+
+        'h1_content_title' => 'h1#content-title',
+        'div_recurrent_vehicle_slider' => 'div#content-vehicle-slider',
+        'div_recurrent_carousel_vehicles' => 'div#content_carousel_vehicles',
+        'div_view_results_list' => 'div#view-results-list',
+        "div_content_privacy" => "div#content_privacy",
+        "div_content_form_sem" => "div#content_form_sem",
+        "div_content_form_job_board" => "div#content_form_job_board",
+
+        // ABOUT US
+        "link_go_about_us" => 'a#go-about-us',
+        "div_recurrent_about_us" => 'div#content-about-us',
+
+        // FOMR CONTACT
+        'form_recurrent_contact_pre_owned' => '#form_sem_premium_contact',
+        'input_contact_pre_owned_name' => '#sem_premium_contact_name',
+        'input_contact_pre_owned_email' => '#sem_premium_contact_email',
+        'input_contact_pre_owned_message' => '#sem_premium_contact_message',
+        'send_contact_pre_owned' => '#sem_premium_contact_send',
+
+        // FORM COMTACT BY MODEL
+        'div_recurrent_form_contact_by_model' => '#content_form_contact_by_model',
+        'form_recurrent_contact_by_model_pre_owned' => '#form_sem_premium_contact_by_model',
+        'input_contact_by_model_pre_owned_name' => '#sem_premium_by_model_contact_name',
+        'input_contact_by_model_pre_owned_email' => '#sem_premium_by_model_contact_email',
+        'input_contact_by_model_pre_owned_phone' => '#sem_premium_by_model_contact_phone',
+        'input_contact_by_model_pre_owned_message' => '#sem_premium_by_model_contact_message',
+        'send_contact_by_model_pre_owned' => '#sem_premium_contact_send_by_model',
+
+        // FORM JOB BOARD
+        'div_recurrent_content_job_board' => '#content_form_job_board',
+        'div_recurrent_content_input_file' => '#content_custom_file_upluad',
+        'form_recurrent_form_job_board' => '#form_job_board',
+        'input_job_board_first_name' => '#job_board_first_name',
+        'input_job_board_last_name' => '#job_board_last_name',
+        'input_job_board_email' => '#job_board_email',
+        'input_job_board_phone' => '#job_board_phone',
+        'input_job_board_interest_area' => '#job_board_interest_area',
+        'input_job_board_message' => '#job_board_message',
+        'input_job_board_upload_file' => 'input#job_board_upload_file',
+        'send_btn_job_board' => '#job_board_send',
+
+        'validate_required' => '.validate-required',
+        'email_from' => '#email-from',
+        'form_wrapper' => '#form-wrapper',
+        'form_thanks' => '.form-thanks',
+        'form_error' => '.form-error',
+        'form_loader' => '.form-loader',
+
+        // NEWSLETTER
+
+        'input_newsletter_email' => '#mce-EMAIL',
+        'input_newsletter_first_name' => '#mce-FNAME',
+        'input_newsletter_last_name' => '#mce-LNAME',
+
+        // WORKSHOP
+        "link_go_workshop" => 'a#go-workshop',
+        "ide_go_workshop" => 'div#id-go-workshop',
+        "div_recurrent_workshop" => 'div#content-workshop',
+        "div_content_workshop_marcs" => 'div#content-workshop-marcs',
+        "div_content_workshop_agencies" => 'div#content-workshop-agencies',
+
+        // CONTENT AGENCIES PRE-OWNED
+
+        'div_recurrent_agencies_preowned' => 'div#content-agencies-preowned',
+        'div_recurrent_agencies_pre_owned_map' => 'div#content-agencies-pre-owned-map',
+
+
+        "section_agencies_preowned_tabs" => "div#agencies-preowned-section-tabs",
+        'div_recurrent_tabs_agencies_preowned' => 'div#content-tabs-agencies-preowned',
+        // GO ACTION AGENCIE PRE OWNED
+        'action_go_agencie_preowned' => 'a.go-action-agencie-preowned',
+        'action_go_agencie_preowned_id' => 'a#go-action-agencie-preowned-',
+        // GO ACTION AGENCIE NEWS
+        'div_recurrent_content_agencies_news_map' => 'div#content-agencies-news-map',
+
+        'action_go_agencie_news' => 'a.go-action-agencie-news',
+        'action_go_agencie_news_id' => 'a#go-action-agencie-news-',
+        // CONTENT AGENCIES NEWS
+        'div_recurrent_agencies_news' => 'div#content-agencies-news',
+        "div_agencies_news_section_tabs" => 'div#agencies-news-section-tabs',
+        "div_content_filters_agencies_news" => 'div#content-filters-agencies-news',
+
+        // AGENCIES NEWS
+
+        // ACTION NEW AGENCIE
+        'action_new_agn' => '.action-new-agn',
+        'div_recurrent_brand_agencies_news' => 'div#content-brand-agencies-news',
+        //
+        "li_agencie_name" => 'li#agencie-name',
+        "li_filter_agencie" => 'li#filter-agencie',
+        "link_go_agencies_news" => 'a#go-agencies-news',
+        "a_go_section_filter_agencies_news" => 'a#go-section-filter-agencies-news',
+        // AGENCIES PRE OWNED
+        'bread_agencie_name_preowned' => 'li#agencie-name-preowned',
+        'bread_filter_agencie_preowned' => 'li#filter-agencie-preowned',
+        // BRAND AGENCIES NEWS CONTENT
+        'div_recurrent_brand_agencies_news' => '#content-brand-agencies-news',
+        // AGENCIES NEWS
+        'bread_agencie_name_news' => 'li#agencie-name-news',
+        'bread_agencie_news' => 'li#agencie-news',
+        'bread_filter_agencie_news' => 'li#filter-agencie-news',
+        'bread_filter_agencie_news_principal' => 'li#filter-agencie-news-principal',
+        'div_recurrent_agencies_news' => 'div#content-agencies-news',
+
+
+        "div_recurrent_send_agencies_news" => 'div#content-send-agencies-news',
+
+        'div_recurrent_block_agencies_news' => 'div#content-agencies-news-block',
+        "ul_filters_link" => '#filter a',
+        "div_ul_option_set" => '#options .option-set',
+
+        // GOOGLE MAPS CONTENT AGENCIES NEWS
+        "div_content_section_map_all_agencies_news" => 'div#content-section-map-all-agencies-news',
+        // GOOGLE MAPS CONTENT AGENCIES
+        "div_recurrent_agencies_map" => 'div#content-agencies-map',
+
+        'div_recurrent_loader_contact' => 'div#loader',
+        'div_recurrent_loader_contact_by_model' => 'div#loader-by-model',
+
+        'div_recurent_map' => 'div#content_map',
+        'div_recurrent_content_mark_map' => 'div#content_mark_map',
+
+        "nav_mobile_toggle" => '.mobile-toggle',
+        "ide_current_home_link" => '#home-link',
+        "ide_current_sem_contact" => '#sem-contact',
+        "vin_go_privacy_notice" => '#go-privacy',
+        //Views Grid / list
+        'id_results_holder' => 'div#results-holder',
+        'class_result_item' => '.result-item',
+        'id_views_list' => '#results-list-view',
+        'id_views_grid' => '#results-grid-view',
+        // BUTTON SHOW TABS AGENICES
+        'button_show_agencies_tabs' => '#Show-Agencies-Tabs',
+        'button_search_tabs' => '#Search-tabs',
+        'li_agencies' => '.agencia',
+
+        // BUTTON SOW FILTER
+        'button_show_filters' => '#Show-Filters',
+        'button_search_filters' => '#Search-Filters',
+
+        // CONTENT SEARCH FILTERS
+        "div_sem_filters_section" => 'div#sem-filters-section',
+        'form_search_sem' => 'form#search-sem',
+        // CONTENT SELECTS
+        "div_select_category" => 'div#div-select-category',
+        "div_select_marc" => 'div#div-select-marc',
+        "div_select_model" => 'div#div-select-model',
+        // FILTER SELECT ACTION
+        "select_filter_action_cat" => 'select.filter-action-cat',
+        "select_filter_action_cat_id" => 'select#filter-action-cat-',
+        "select_filter_action_mrc" => 'select.filter-action-mrc',
+        "select_filter_action_mrc_id" => 'select#filter-action-mrc-',
+        "select_filter_action_mdo" => 'select.filter-action-mdo',
+        "select_filter_action_mdo_id" => 'select#filter-action-mdo-',
+        // FILTER INPUT ACTION
+        "input_filter_action_year_min" => 'input.filter-action-year-min',
+        "input_filter_action_year_min_id" => 'input#filter-action-year-min-',
+        "input_filter_action_year_max" => 'input.filter-action-year-max',
+        "input_filter_action_year_max_id" => 'input#filter-action-year-max-',
+        "input_filter_action_price_min" => 'input.filter-action-price-min',
+        "input_filter_action_price_min_id" => 'input#filter-action-price-min-',
+        "input_filter_action_price_max" => 'input.filter-action-price-max',
+        "input_filter_action_price_max_id" => 'input#filter-action-price-max-',
+        // FORM FOUND ELEMENTS
+        'select_sem_category' => 'select#sem-category',
+        'select_sem_marc' => 'select#sem-marc',
+        'select_sem_model' => 'select#sem-model',
+        'input_sem_year_min' => 'input#sem-year-min',
+        'input_sem_year_max' => 'input#sem-year-max',
+        'input_sem_price_min' => 'input#sem-price-min',
+        'input_sem_price_max' => 'input#sem-price-max',
+        // BUTTON FOUND
+        'button_sem_search_action' => 'a.sem-search-action',
+        'button_sem_search_action_id' => 'a#sem-search-action-',
+        // VIEW DETAILS
+        'link_sem_action' => 'a.sem-action',
+        'link_sem_action_id' => 'a#sem-action-',
+
+        // Valida Campos
+       'input_validate_required' => 'input.validate-required',
+       'input_validate_email' => 'input.validate-email',
+
+        // NOTICIAS
+       'go_news' => 'a#go-news',
+       'go_news_volvo' => 'a#go-news-volvo',
+       'go_news_ldr' => 'a#go-news-ldr',
+       'go_news2_ldr' => 'a#go-news2-ldr',
+       'go_news3_ldr' => 'a#go-news3-ldr',
+       'go_news_jag' => 'a#go-news-jag',
+
+       'div_recurrent_address' => 'div#content-address',
+
+       // inventories
+       'div_recurrent_results_list' => 'div#view-results-list'
+
+
+    );
+}
