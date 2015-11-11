26eb35d0cba23d53826cae0a534f695f88e813df
Heriberto Velasco Mora <hevelmo060683@gmail.com>
2015-11-11 12:55:28 -0600

    update templates

--- templates/templates.js
+++ templates/templates.js
@@ -1,10 +1,10 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['tmp_demo'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+templates['tmp_demo'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<br />\r\n<div align='center'>\r\n	<select id='lan-demo'>\r\n		<option value='es'>Español</option>\r\n		<option value='en'>English</option>\r\n	</select>\r\n	<h3 id='demo-date' data-date='"
-    + this.escapeExpression(this.lambda(((stack1 = (depth0 != null ? depth0.propa : depth0)) != null ? stack1.date : stack1), depth0))
-    + "'><h3>\r\n</div>\r\n<script>\r\n	var date;\r\n	date = $('h3#demo-date').data('date');\r\n	newDate = PRO.momentToRoman(date, 'es');\r\n	$('h3#demo-date').text(newDate);\r\n</script>\r\n";
+  return "<br />\n<div align='center'>\n	<select id='lan-demo'>\n		<option value='es'>Español</option>\n		<option value='en'>English</option>\n	</select>\n	<h3 id='demo-date' data-date='"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.propa : depth0)) != null ? stack1.date : stack1), depth0))
+    + "'><h3>\n</div>\n<script>\n	var date;\n	date = $('h3#demo-date').data('date');\n	newDate = PRO.momentToRoman(date, 'es');\n	$('h3#demo-date').text(newDate);\n</script>\n";
 },"useData":true});
 })();
