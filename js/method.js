91ef809eb08b5608245567165dde6751c3dc9a1c
Heriberto Velasco Mora <hevelmo060683@gmail.com>
2015-11-11 18:25:35 -0600

    change

--- js/method.js
+++ js/method.js
@@ -7,14 +7,6 @@
     var multipleSupport = typeof $('<input/>')[0].multiple !== 'undefined',
         isIE = /msie/i.test( navigator.userAgent );
 /* ------------------------------------------------------ *\
-    [functions] validateEmail
-\* ------------------------------------------------------ */
-    function validateEmail(email) {
-        var re = /^(([^<>()[\]\\., ;:\s@\"] + (\.[^<>()[\]\\., ;:\s@\"] + )*)|(\". + \"))@((\[[0-9] {1, 3}\.[0-9] {1, 3}\.[0-9] {1, 3}\.[0-9] {1, 3}\])|(([a-zA-Z\-0-9] + \.) + [a-zA-Z] {2, }))$/;
-        //return re.test(email);
-        return true;
-    }
-/* ------------------------------------------------------ *\
     [functions] Alert Custom
 \* ------------------------------------------------------ */
     function resetAlert () {
@@ -72,5 +64,138 @@
             date = $(domEl.h3_demo_date).data('date');
             newDate = PRO.momentToRoman(date, lan);
             $(domEl.h3_demo_date).text(newDate);
+        }
+    }
+/* ------------------------------------------------------ *\
+    [Methods] inputVal
+\* ------------------------------------------------------ */
+    var inputValMetdods = {
+        isIntegerKP: function (event) {
+            return /\d/.test(String.fromCharCode(event.keyCode));
+        }
+    }
+/* ------------------------------------------------------ *\
+    [Methods] validations_regexp
+\* ------------------------------------------------------ */
+    var validations_regexp = {
+        address : new RegExp( /^[a-zá-úüñ,#0-9. -]{2,}$/i ),
+        date    : new RegExp( /^(\d{4})-(\d{1,2})-(\d{1,2})$/ ),
+        email   : new RegExp( /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ),
+        name    : new RegExp( /^[a-zá-úüñ. ]{2,}$/i ),
+        phone   : new RegExp( /^[0-9\s\-]{7,13}$/ )
+    }
+/* ------------------------------------------------------ *\
+    [Methods] validation_messages
+\* ------------------------------------------------------ */
+    var validation_messages = {
+        date            : 'Debe ser aaaa-mm-dd',
+        date_tomorrow   : 'Sólo a partir de mañana',
+        email           : 'Verifica tu correo',
+        general         : 'Campo no válido',
+        not_config      : 'Tipo desconocido',
+        not_null        : 'No puede ser nulo',
+        phone           : 'Verifica que tu número sea de 10 dígitos',
+        required        : 'Campo requerido',
+        empty           : 'Campo vacío'
+    }
+/* ------------------------------------------------------ *\
+    [Methods] validate
+\* ------------------------------------------------------ */
+    var validateMethods = {
+        validate : function(value, rules, required, custom_message) {
+            var r = { valid : false, message : '' },
+            null_value = value == undefined || value === '' ,
+            ii, rule;
+            required = required === true ? true: false;
+            if( required ){
+                if( null_value ){
+                    r.message = validation_messages.required;
+                }
+            }else{
+                if( null_value ){
+                    r.valid = true;
+                }
+            }
+            if( !r.valid && r.message === '' ){
+                ii = rules.length;
+                while( ii-- ){
+                    rule = rules[ii];
+                    switch( rule ){
+                        case 'email':
+                            if( !validations_regexp.email.test( value ) ){
+                                r.message = validation_messages.email;
+                            }
+                            break;
+                        case 'name':
+                            if( !validations_regexp.name.exec( value ) ){
+                                r.message = validation_messages.general;
+                            }
+                            break;
+                        case 'address':
+                            if( !validations_regexp.address.exec( value ) ){
+                                r.message = validation_messages.general;
+                            }
+                            break;
+                        case 'grade':
+                            if(  !is_model_name( value ) ){
+                                r.message = validation_messages.required;
+                            }
+                            break;
+                        case 'date':
+                            if( !validations_regexp.date.exec( value ) ){
+                                r.message = validation_messages.date;
+                            }
+                            break;
+                        case 'phone':
+                            if( !validations_regexp.phone.exec( value ) ){
+                                r.message = validation_messages.phone;
+                            }
+                            break;
+                        default:
+                            r.message = validations_regexp.not_config;
+                            break;
+                    }
+                }
+                if( r.message === '' ){
+                    r.valid = true;
+                }
+            }
+            if( custom_message && !r.valid ){
+                r.message = custom_message;
+            }
+            return r;
+        },
+        //Display Input errors
+        error_bubble : function( $label, show, message ){
+            var $p = $label.parent().children('p.invalid-message');
+            if( show ){
+                if( message ){
+                    $p.html( message + '<span>&nbsp;</span>' ).stop().hide().fadeIn();
+                }else{
+                    $p.stop().hide().fadeIn();
+                }
+            }else{
+                $p.hide();
+            }
+        },
+        validate_input : function(event) {
+            var target = $(event.target);
+            //console.log(target);
+            if( target.is('input') || target.is('textarea') ){
+                var valid_data = target.data('validation-data');
+                var val_data    = valid_data.split('|'),
+                    required    = val_data.indexOf('required');
+                if( required >= 0 ){
+                    val_data.splice(required, 1);
+                }
+                var value = target.val(),
+                    validation = validateMethods.validate( value, val_data, ( required >= 0 )  );
+                validateMethods.error_bubble( target, !validation.valid, validation.message );
+                return validation.valid;
+            }else{
+                var is_valid = !( target.val() === null );
+                validateMethods.error_bubble( target, !is_valid, validation_messages.required );
+                return is_valid;
+            }
         }
     }
